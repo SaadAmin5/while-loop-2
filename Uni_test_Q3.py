f2cd37e583a6fbf79040615c88548d9e712d1d9c
Saad Amin <44711776+Saad55555@users.noreply.github.com>
2024-02-02 06:08:09 +0500

    Update Uni_test_Q3.py

--- Uni_test_Q3.py
+++ Uni_test_Q3.py
@@ -1,18 +1,3 @@
-'''Question 3
-
-Write a Python program that reads a series of positive integer numbers one by one from the 
-keyboard (ending by -1) and, at the end, outputs 'Yes' once if the numbers were in (strictly) 
-ascending order, otherwise it outputs 'No' (again, only once and at the end).
-
-For example, it should output 'Yes' for 3, 5, 8, 15, 22, -1, and for 10, 20, 45, -1; 
-but ‘No’ for 3, 5, 4, 8, -1, for 1, 2, 1, -1, and for 3, 5, 5, 8, -1.
-
-You may assume that the user enters at least two positive numbers before the -1 and 
-that there are no incorrect inputs.
-'''
-
-#solution
-
 list1=[]
 
 while True:
